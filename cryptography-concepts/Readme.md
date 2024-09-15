78915f01eb235cc93dcb6846c4dd873dcf8f4b86
Yash <yashpc09@gmail.com>
2024-09-15 10:51:10 +0530

    salt

--- cryptography-concepts/Readme.md
+++ cryptography-concepts/Readme.md
@@ -4,4 +4,11 @@ cryptography is a process of securing data using some algorithm.
 
 ## Hash
 
-same input -> same output
+same-input -> same-output
+With hash it is easy to observe patterns between different hashes and commonly used value, this thing makes it a bit unsecure.
+Hashing: Transforms data into a fixed-size string of characters, typically used for verifying the integrity of data and securely storing passwords.
+
+## Salt
+
+same input -> random-salt + same-output
+Salting: Adds random data to a password before hashing, providing increased protection from attacks like rainbow table lookups.
---
+++ cryptography-concepts/src/salt.js
@@ -0,0 +1,35 @@
+const { scryptSync, randomBytes, timingSafeEqual } = require("crypto");
+
+function signup(email, password) {
+  const salt = randomBytes(16).toString("hex");
+  const hashedPassword = scryptSync(password, salt, 64).toString("hex");
+
+  const user = { email, password: `${salt}:${hashedPassword}` };
+  return user;
+}
+
+function login(email, password) {
+  const user = users.find((v) => v.email === email);
+
+  const [salt, key] = user.password.split(":");
+  const hashedBuffer = Buffer.from(key, "hex");
+
+  const keyBuffer = Buffer.from(key, "hex");
+  const match = timingSafeEqual(hashedBuffer, keyBuffer);
+
+  if (match) {
+    return "login success!";
+  } else {
+    return "login fail!";
+  }
+}
+
+let users = [];
+const email = "johndoe@123.mail.com";
+const password = "john-is-doe";
+
+const user = signup(email, password);
+users.push(user);
+const match = login(email, password);
+console.log(match);
+console.log(user);
